2da78043b723224007893efd2e62ae6dca209ae1
Q-holi <32233160+Q-holi@users.noreply.github.com>
2024-02-28 11:52:41 +0900

    PlayerData Save and Load

--- Assets/Public Scripts/Inventory.cs
+++ Assets/Public Scripts/Inventory.cs
@@ -3,10 +3,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements.Experimental;
-using static UnityEditor.Progress;
+
 
 [Serializable]
 public class Inventory : MonoBehaviour
@@ -24,47 +22,6 @@ public class Inventory : MonoBehaviour
     }
     private void OnEnable()
     {
-        if (File.Exists(collectionJsonFilePath))
-        {
-            string jsonText = File.ReadAllText(collectionJsonFilePath);
-            // JSON 문자열을 Deserialize하여 List에 추가
-            //collections.AddRange(JsonConvert.DeserializeObject<List<Collection>>(jsonText));
-            items.AddRange(JsonConvert.DeserializeObject<List<Item>>(jsonText));
-        }
 
-        Collection data1 = new Collection();
-        data1.keyvalue = 1001;
-        data1.name = "Test";
-        data1.rating = Rating.Normal;
-        data1.texture2DImagePath = "";
-        data1.count = 1001;
-        data1.red_Option = 6;
-        data1.black_Option = 8;
-
-        collections.Add(data1);
-        Debug.Log(collections[0].name);
-        string json = JsonUtility.ToJson(collections);
-        File.WriteAllText(collectionJsonFilePathtest, json);
-    }
-    private void OnApplicationQuit()
-    {
-
-        //SaveCollectionsToJson();
-    }
-
-    void SaveInventory()
-    {
-
-    }
-    public void SaveCollectionsToJson()
-    {
-        List<string> serializedCollections = new List<string>();
-        foreach (Collection collection in collections)
-        {
-            serializedCollections.Add(collection.Serialize());
-        }
-        string json = JsonUtility.ToJson(serializedCollections);
-        File.WriteAllText(collectionJsonFilePathtest, json);
-        // 저장 로직
     }
 }
