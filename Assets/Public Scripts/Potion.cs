8d13fa5b19dcb0e02a942b7eb6d9ddfd0382d214
Cenchi185 <kangmin7840@naver.com>
2024-03-21 10:47:28 +0900

    Potion Modify

--- Assets/Public Scripts/Potion.cs
+++ Assets/Public Scripts/Potion.cs
@@ -7,10 +7,17 @@ public class Potion : Item
 {
     public int amount;
     public int duration;
+    public List<BasePotionData> enchantPotions = new List<BasePotionData>(); 
 
     public Potion(BasePotionData data) : base(data)
     {
+        enchantPotions.Add(data);
         amount = data.amount;
         duration = data.duration;
+    }
+
+    public void EnchantPotion(BasePotionData data)
+    {
+        enchantPotions.Add(data);
     }
 }
