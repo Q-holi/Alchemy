fb7e20e9e809a22e07322cfe96b79d8b4ea1a087
Q-holi <32233160+Q-holi@users.noreply.github.com>
2024-03-26 11:56:31 +0900

    아이템 클래스 수정

--- Assets/Public Scripts/ItemData Scripts/BaseCollectionData.cs
+++ Assets/Public Scripts/ItemData Scripts/BaseCollectionData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [CreateAssetMenu(fileName = "Collection", menuName = "Inventory System/Collection", order = 1)]
 public class BaseCollectionData : BaseItemData
 {
